ed29c87be66cdab69ca386e16273100b84353ae6
Asdamar <electrostaticcharge@gmail.com>
2018-05-14 21:53:41 +0200

    Auth testing... 123

--- WatchWebsite/App_Code/Models/CustomerModel.cs
+++ WatchWebsite/App_Code/Models/CustomerModel.cs
@@ -14,4 +14,55 @@ public class CustomerModel
         // TODO: Add constructor logic here
         //
     }
+
+    public String InsertCustomer(Customer customer)
+    {
+        try
+        {
+            WatchDBEntities wdb = new WatchDBEntities();
+            wdb.Customers.Add(customer);
+            wdb.SaveChanges();
+
+            return "Add Successful";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public String UpdateCustomer(int id, Customer customer)
+    {
+        try
+        {
+            ///
+
+            return "Update Successful";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public String DeleteCustomer(int id)
+    {
+        try
+        {
+            WatchDBEntities wdb = new WatchDBEntities();
+
+            Customer customer = wdb.Customers.Find(id);
+
+            wdb.Customers.Attach(customer);
+            wdb.Customers.Remove(customer);
+
+            wdb.SaveChanges();
+
+            return "Delete Successful";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
 }
--- WatchWebsite/App_Code/Models/TransactionModel.cs
+++ WatchWebsite/App_Code/Models/TransactionModel.cs
@@ -14,4 +14,62 @@ public class TransactionModel
         // TODO: Add constructor logic here
         //
     }
+
+    public String InsertTransaction(Transaction transaction)
+    {
+        try
+        {
+            WatchDBEntities wdb = new WatchDBEntities();
+            wdb.Transactions.Add(transaction);
+            wdb.SaveChanges();
+
+            return "Add Successful";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public String UpdateBattery(int id, Battery battery)
+    {
+        try
+        {
+            WatchDBEntities wdb = new WatchDBEntities();
+
+            Battery b = wdb.Batteries.Find(id);
+
+            b.Battery1 = battery.Battery1;
+            b.Branch = battery.Branch;
+
+            wdb.SaveChanges();
+
+            return "Update Successful";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public String DeleteTransaction(int id)
+    {
+        try
+        {
+            WatchDBEntities wdb = new WatchDBEntities();
+
+            Transaction transaction = wdb.Transactions.Find(id);
+
+            wdb.Transactions.Attach(transaction);
+            wdb.Transactions.Remove(transaction);
+
+            wdb.SaveChanges();
+
+            return "Delete Successful";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
 }
