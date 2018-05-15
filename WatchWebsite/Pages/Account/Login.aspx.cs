775b9c397299dcad93ce6666b66568a19d201852
Asdamar <mnov5@student.monash.edu>
2018-05-15 23:02:35 +0200

    latest

--- WatchWebsite/Pages/Account/Login.aspx.cs
+++ WatchWebsite/Pages/Account/Login.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +17,31 @@ public partial class Pages_Account_Login : System.Web.UI.Page
 
     protected void Button2_Click(object sender, EventArgs e)
     {
+        UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
 
+        userStore.Context.Database.Connection.ConnectionString =
+            System.Configuration.ConfigurationManager.ConnectionStrings["WatchDBv2EntitiesConnectionString"].ConnectionString;
+
+
+        UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+
+        var user = manager.Find(txtUsrName.Text, txtPassword.Text);
+
+        if (user != null)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+            authenticationManager.SignIn(new AuthenticationProperties
+            {
+                IsPersistent = false
+            }, userIdentity);
+
+            Response.Redirect("~/Index.aspx");
+        }
+        else
+        {
+            litStatus.Text = "Invalid username or password";
+        }
     }
 }
