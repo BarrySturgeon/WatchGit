a43e8e75ae4a866b83a350140ab6091a6303e947
Barry <bjstu3@student.monash.edu>
2018-08-02 20:38:35 +0200

    A basic watch repair page

--- WatchWebsite/Pages/BookRepair.aspx.cs
+++ WatchWebsite/Pages/BookRepair.aspx.cs
@@ -9,6 +9,21 @@ public partial class Pages_BookRepair : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        lbResult.Text = string.Empty;
+    }
+
+    protected void btnSub_Click(object sender, EventArgs e)
+    {
+
+        foreach (ListItem item in chBox.Items)
+        {
+            if (item.Selected) {
+
+                lbResult.Text += item.Text + ": price for fixing ranges from R " + item.Value + "<br>";
+
+            }
+
+        }
 
     }
 }
