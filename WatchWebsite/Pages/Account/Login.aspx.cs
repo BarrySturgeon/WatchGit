5a2dd3ec685404f1a8cbcfaf0867ee51e334c893
Asdamar <electrostaticcharge@gmail.com>
2018-08-30 17:41:18 +0200

    yesus

--- WatchWebsite/Pages/Account/Login.aspx.cs
+++ WatchWebsite/Pages/Account/Login.aspx.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
@@ -8,86 +8,75 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Pages_Account_Login : System.Web.UI.Page
+public partial class Pages_Account_Register : System.Web.UI.Page
 {
-    protected void Page_Load(Object sender, EventArgs e)
+    protected void Page_Load(object sender, EventArgs e)
     {
 
     }
 
-    protected void Button2_Click(Object sender, EventArgs e)
+    protected void Button1_Click(object sender, EventArgs e)
     {
+        // API to create user
 
+        UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+
+        userStore.Context.Database.Connection.ConnectionString =
+            System.Configuration.ConfigurationManager.ConnectionStrings["WatchDBv2ConnectionString"].ConnectionString;
+
+
+        UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+
+
+        // Creating the user
+        IdentityUser user = new IdentityUser();
+        user.UserName = txtUserName.Text;
+
+        if (txtPassword.Text == txtConfirmPassword.Text)
+        {
+            try
+            {
+                IdentityResult result = manager.Create(user, txtPassword.Text);
+                if (result.Succeeded)
+                {
+
+                    UserInformation info = new UserInformation
+                    {
+
+                        Address = txtAddress.Text,
+                        FirstName = txtFirstName.Text,
+                        LastName = txtLastName.Text,
+                        GUID = user.Id
+                    };
+
+                    UserInfoModel model = new UserInfoModel();
+                    model.InsertUserInformation(info);
+
+                    // Store in DB
+                    var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+
+                    // Set to log in by Cookie
+                    var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    litStatus.Text = "Registration successful!";
+
+                    // Redirect to home page
+                    authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+                    Response.Redirect("~/Pages/Index.aspx");
+                }
+                else
+                {
+                    litStatus.Text = result.Errors.FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                litStatus.Text = ex.Message;
+            }
+        }
+        else
+        {
+            litStatus.Text = "Passwords do not match";
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //protected void Page_Load(object sender, EventArgs e)
-    //{
-    //    if(Page.PreviousPage != null && Page.PreviousPage.Request.Url == new Uri("~/Pages/Management/Management.aspx", UriKind.Absolute) )
-    //    /* "~/Pages/Management/Management.aspx" */
-    //    {
-    //        throw new HttpException(403, "Forbidden");
-
-    //        //Response.StatusCode = 403;
-    //        //Response.End();
-
-    //        // code not functioning properly
-    //    }
-    //}
-
-    //protected void Button2_Click(object sender, EventArgs e)
-    //{
-    //    UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
-
-    //    userStore.Context.Database.Connection.ConnectionString =
-    //        System.Configuration.ConfigurationManager.ConnectionStrings["WatchDatabaseV4Entities"].ConnectionString;
-
-
-    //    UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
-
-    //    var user = manager.Find(txtUsrName.Text, txtPassword.Text);
-
-    //    if (user != null)
-    //    {
-    //        var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
-    //        var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
-
-    //        authenticationManager.SignIn(new AuthenticationProperties
-    //        {
-    //            IsPersistent = false
-    //        }, userIdentity);
-
-    //        Response.Redirect("~/Pages/Index.aspx");
-    //    }
-    //    else
-    //    {
-    //        litStatus.Text = "Invalid username or password";
-    //    }
-    //}
 }
