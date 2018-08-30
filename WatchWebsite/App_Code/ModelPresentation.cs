224412d7f05a5fe95c5d1c7c4a705efcc38481fb
Asdamar <electrostaticcharge@gmail.com>
2018-08-30 17:27:11 +0200

    reverting to workable solution
    pretty much...

--- WatchWebsite/App_Code/ModelPresentation.cs
+++ WatchWebsite/App_Code/ModelPresentation.cs
@@ -10,6 +10,79 @@
 using System;
 using System.Collections.Generic;
 
+public partial class AspNetRole
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+    public AspNetRole()
+    {
+        this.AspNetUsers = new HashSet<AspNetUser>();
+    }
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+    public virtual ICollection<AspNetUser> AspNetUsers { get; set; }
+}
+
+public partial class AspNetUser
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+    public AspNetUser()
+    {
+        this.AspNetUserClaims = new HashSet<AspNetUserClaim>();
+        this.AspNetUserLogins = new HashSet<AspNetUserLogin>();
+        this.AspNetRoles = new HashSet<AspNetRole>();
+    }
+
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public string PasswordHash { get; set; }
+    public string SecurityStamp { get; set; }
+    public string PhoneNumber { get; set; }
+    public bool PhoneNumberConfirmed { get; set; }
+    public bool TwoFactorEnabled { get; set; }
+    public Nullable<System.DateTime> LockoutEndDateUtc { get; set; }
+    public bool LockoutEnabled { get; set; }
+    public int AccessFailedCount { get; set; }
+    public string UserName { get; set; }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+    public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+    public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; }
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+    public virtual ICollection<AspNetRole> AspNetRoles { get; set; }
+}
+
+public partial class AspNetUserClaim
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string ClaimType { get; set; }
+    public string ClaimValue { get; set; }
+
+    public virtual AspNetUser AspNetUser { get; set; }
+}
+
+public partial class AspNetUserLogin
+{
+    public string LoginProvider { get; set; }
+    public string ProviderKey { get; set; }
+    public string UserId { get; set; }
+
+    public virtual AspNetUser AspNetUser { get; set; }
+}
+
+public partial class C__MigrationHistory
+{
+    public string MigrationId { get; set; }
+    public string ContextKey { get; set; }
+    public byte[] Model { get; set; }
+    public string ProductVersion { get; set; }
+}
+
 public partial class Cart
 {
     public int ID { get; set; }
@@ -55,6 +128,15 @@ public partial class ProductType
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
     public virtual ICollection<Product> Products { get; set; }
+}
+
+public partial class sysdiagram
+{
+    public string name { get; set; }
+    public int principal_id { get; set; }
+    public int diagram_id { get; set; }
+    public Nullable<int> version { get; set; }
+    public byte[] definition { get; set; }
 }
 
 public partial class UserInformation
